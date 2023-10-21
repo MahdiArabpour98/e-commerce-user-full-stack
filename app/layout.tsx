6bf6beacf27408c0572e435758f27ac2b57326b2
MahdiArabpour <85918072+MahdiArabpour98@users.noreply.github.com>
2023-10-21 20:17:30 +0330

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 
+import Footer from "@/components/footer";
+
 const font = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -12,7 +14,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
