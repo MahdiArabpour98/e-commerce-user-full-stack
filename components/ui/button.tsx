68299fe7a76ab6bd01c79b8c37f35f5282d0b40b
MahdiArabpour <85918072+MahdiArabpour98@users.noreply.github.com>
2023-10-23 13:53:10 +0330

    create modal and cart storage

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -7,6 +7,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, disabled, type = "button", ...props }, ref) => {
     return (
       <button
+        {...props}
         ref={ref}
         className={cn(
           `
---
+++ providers/modal-provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import PreviewModal from "@/components/preview-modal";
+
+import { useState, useEffect } from "react";
+
+const ModalProvider = () => {
+  const [ismounted, setIsmounted] = useState(false);
+
+  useEffect(() => {
+    setIsmounted(true);
+  }, []);
+
+  if (!ismounted) {
+    return null;
+  }
+  return (
+    <>
+      <PreviewModal />
+    </>
+  );
+};
+
+export default ModalProvider;
